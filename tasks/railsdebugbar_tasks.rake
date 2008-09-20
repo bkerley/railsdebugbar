0a7a024419646d13ade713a576c9fc9c172a0e15
Bryce Kerley <bkerley@brycekerley.net>
2008-09-20 19:22:32 -0400

    stub plugin

---
+++ tasks/railsdebugbar_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :railsdebugbar do
+#   # Task goes here
+# end
---
+++ test/railsdebugbar_test.rb
@@ -0,0 +1,8 @@
+require 'test/unit'
+
+class RailsdebugbarTest < Test::Unit::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
