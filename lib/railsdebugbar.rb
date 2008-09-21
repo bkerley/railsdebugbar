7de229e842a728981856403af8eebd8b7c3c2737
Bryce Kerley <bkerley@brycekerley.net>
2008-09-20 21:43:22 -0400

    fancied up, rails_version display

--- lib/railsdebugbar.rb
+++ lib/railsdebugbar.rb
@@ -10,7 +10,39 @@ class RailsDebugBar
 			insertpoint = -1
 		end
 		
-		bar = "DEBUG"
-		controller.response.body = body.insert(insertpoint, bar)
+		parts = ["DEBUG",
+			rails_version
+		]
+		
+		
+		
+		controller.response.body = body.insert(insertpoint, decorate_parts(parts))
+	end
+	
+	def self.rails_version
+		"Rails: "+Rails::VERSION::STRING
+	end
+	
+	def self.decorate_parts(parts)
+		css_for_insertion = <<-EOF
+		<style>
+		ul#rails_debug_bar {
+			list-style-type: none;
+			margin-bottom: 0;
+			margin-top: 1em;
+			padding-left: 0
+		}
+		ul#rails_debug_bar li {
+			display: inline;
+			padding: 0.2em;
+			border: 1px solid black;
+			background-color: #fda;
+			color: black;
+		}
+		</style>
+		EOF
+		parts_for_insertion = parts.map{|s| "<li>#{s}</li>"}
+		html_for_insertion = "<ul id=\"rails_debug_bar\">#{parts_for_insertion}</ul>"
+		return css_for_insertion + html_for_insertion
 	end
 end
