9d5e9653555c42da72ac90668018470a8deb8fe3
Bryce Kerley <bkerley@brycekerley.net>
2008-09-20 21:24:38 -0400

    stub

--- lib/railsdebugbar.rb
+++ lib/railsdebugbar.rb
@@ -1,1 +1,16 @@
-# Railsdebugbar
+class RailsDebugBar
+	def self.filter(controller)
+		content_type = controller.response.content_type
+		return unless content_type =~ /html/
+		
+		body = controller.response.body
+		
+		insertpoint = (body =~ /<\/body>/)
+		if insertpoint.nil?
+			insertpoint = -1
+		end
+		
+		bar = "DEBUG"
+		controller.response.body = body.insert(insertpoint, bar)
+	end
+end
